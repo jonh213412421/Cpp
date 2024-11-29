6cecd859fb34202ad0f00d141f50a9d711e288a3
João Alves <138532454+jonh213412421@users.noreply.github.com>
2024-11-28 21:00:29 -0300

    Create double.cpp

---
+++ double.cpp
@@ -0,0 +1,13 @@
+//install mingw64 for gmp lib
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+  // range of double
+    double n = 2;
+    while (1) {
+        n = n * 4;
+        printf("%f\n", n);
+    }
+}
