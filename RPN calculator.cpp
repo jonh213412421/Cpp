1d119a04869c554c05393851ad0ac4204c942ed0
João Alves <138532454+jonh213412421@users.noreply.github.com>
2024-12-05 21:07:52 -0300

    Update RPN calculator.cpp

--- RPN calculator.cpp
+++ RPN calculator.cpp
@@ -3,7 +3,7 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
-#define STACK_SIZE 1000
+#define STACK_SIZE 100
 
 
 //size of stack
@@ -54,24 +54,65 @@ int main() {
         printf("entre com um número ou operador\n");
         //if you dont leave the space before %c, it will not read properly
         scanf(" %c", &entry);
-        printf("%c\n", entry);
+        //does the calculation and return result to the stack
         if (entry == '+'||entry == '-'||entry == '/'|| entry == '*') {
-            if (entry == '+') {
-                while (i = top, i > 0, i--) {
-                printf("%c", contents[top]);
-                pop();
-                aux = contents[top];
-                printf("aux: %d\n", aux);
-                sum += aux - '0';
-                printf("soma: %d\n", sum);
-                }
-            }
-        }
-        if (entry == '=') {
 
+            //handles plus
+            if (entry == '+') {
+                printf("top: %d\n", top);
+                while (top > 0) {
+                    pop();
+                    printf("%c\n", contents[top]);
+                    aux = contents[top] - '0';
+                    printf("aux: %d\n", aux);
+                    sum += aux;
+                    printf("top after sum: %d\n", top);
+                    printf("sum at the end of the loop: %d\n", sum);
+                }
+            push(sum + '0');
+            sum = 0;
+            }
+
+            //handles minus
+            if (entry == '-') {
+                printf("top: %d\n", top);
+                while (top >0) {
+                    pop();
+                    printf("%c\n", contents[top]);
+                    aux = contents[top] - '0';
+                    printf("aux: %d\n", aux);
+                    if (top == 0) {
+                        sum = aux + sum;
+                    }
+                    else {
+                        sum -= aux;
+                    }
+                    printf("top after sum: %d\n", top);
+                    printf("sum at the end of the loop: %d\n", sum);
+                }
+            push(sum + '0');
+            sum = 0;
+            }
+
+            //handles times
+            if (entry == '*') {
+                //for the first loop
+                sum = 1;
+                printf("top: %d\n", contents[top]);
+                while (top > 0) {
+                    pop();
+                    printf("%c\n", contents[top]);
+                    aux = contents[top] - '0';
+                    printf("aux: %d\n", aux);
+                    sum *= aux;
+                    printf("top after sum: %d\n", top);
+                    printf("sum at the end of the loop: %d\n", sum);
+                }
+            push(sum + '0');
+            sum = 0;
+            }
         } else {
             push(entry);
         }
     }
-    printf("ok");
 }
