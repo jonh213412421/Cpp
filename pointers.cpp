bbc83390945186c2dd0a13191eeb5e044ea6ade2
João Alves <138532454+jonh213412421@users.noreply.github.com>
2024-12-08 16:22:36 -0300

    Update pointers.cpp

--- pointers.cpp
+++ pointers.cpp
@@ -1,5 +1,6 @@
 //install mingw64 for gmp lib
 //pointers working
+//1
 #include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -15,4 +16,24 @@ int main() {
     *p = 5;
     printf("i: %d\n", i);
     printf("p: %d\n", *p);
+}
+
+
+//install mingw64 for gmp lib
+//soma números em array de inteiros
+//2
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+    int array[] = {1, 6, 13, 30, 25};
+    int n = 5;
+    int *p;
+    int sum = 0;
+    p = &array[0];
+    for (p = &array[0]; p < &array[n]; p++) {
+        sum += *p;
+    }
+    printf("soma: %d", sum);
 }
