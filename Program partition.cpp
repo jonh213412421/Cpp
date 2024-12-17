d99772dacad166391e566b27f77e54e995f32b84
João Alves <138532454+jonh213412421@users.noreply.github.com>
2024-12-17 11:51:03 -0300

    Create Program partition.cpp

---
+++ Program partition.cpp
@@ -0,0 +1,10 @@
+//install mingw64 for gmp lib
+//program partition
+//usa o arquivo "header.h"
+#include <stdio.h>
+#include "header.h"
+
+int main(int argc, char *argv[]) {
+    printf("%d\n", TRUE);
+    printf("%d\n", FALSE);
+}
